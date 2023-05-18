93893230e210ad63666ada5959f8937e282f8121
Gautham Kamath K <gauthamkamathk24@gmail.com>
2023-05-18 15:26:51 +0000

    initial commit

---
+++ src/Categories.js
@@ -0,0 +1,3 @@
+export const categories = [
+    { id: "1", type: "Action" }, { id: "2", type: "Sports" }, { id: "3", type: "Adventure" }, { id: "4", type: "Puzzles" }, { id: "5", type: "Strategy" }
+]
